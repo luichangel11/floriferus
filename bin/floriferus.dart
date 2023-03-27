82c32ea80aafd5365a4a3909ca097db24149fe4d
luis angel <luichtec@gmail.com>
2023-03-27 16:39:08 -0700

    primer commit

---
+++ bin/floriferus.dart
@@ -0,0 +1,5 @@
+import 'package:floriferus/floriferus.dart' as floriferus;
+
+void main(List<String> arguments) {
+  print('Hello world: ${floriferus.calculate()}!');
+}
---
+++ test/floriferus_test.dart
@@ -0,0 +1,8 @@
+import 'package:floriferus/floriferus.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
