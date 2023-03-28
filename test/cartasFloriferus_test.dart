fc035d31423cb724900486b46a0351fa265c83e2
luis angel <luichtec@gmail.com>
2023-03-27 17:11:28 -0700

    2

--- test/cartasFloriferus_test.dart
+++ test/cartasFloriferus_test.dart
@@ -5,3 +5,9 @@ void main() {
     // TODO: Implement test
   });
 }
+
+class cartas{
+  List<String> bounty = ["tulip,mariq,marip","tulip,tulip,daisy","escara,mariq,abeja","lily,mum,tulip",
+  "daisy,mum,abeja","lily,lily,mariq","popy,daisy,escara","daisy,popy,poli","popy,mum,marip"];
+  List<String> flores = ["","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","","",""];
+}
